42f808d7c92a48ef12cc5bf4e783f03c2a1be092
Brian Pugh <bnp117@gmail.com>
2018-09-14 09:13:20 -0700

    benchmarking

--- test/test_mnemonic.c
+++ test/test_mnemonic.c
@@ -5,16 +5,20 @@
 
 #include "unity.h"
 
+#include "sodium.h"
+#include <esp_log.h>
+#include <esp_system.h>
+#include <esp_timer.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <esp_system.h>
-#include "sodium.h"
 
 #include "bipmnemonic.h"
 
 /* Some test vectors were sourced from:
  * https://github.com/trezor/python-mnemonic/blob/master/vectors.json */
+
+static const char TAG[] = "test_mnemonic";
 
 TEST_CASE("BIP39/44 Wordlist Search", "[bip_mnemonic]"){
     char word[10];
@@ -215,14 +219,29 @@ TEST_CASE("BIP39/44 Binary to Mnemonic", "[bip_mnemonic]"){
 }
 
 TEST_CASE("BIP39/44 Mnemonic To Master Seed", "[bip_mnemonic]"){
+    /* Typical Benchmark (uS)
+     * 12-Word
+     * 857,400 - default (sha engine disabled, use mbed implementations)
+     * 1,273,561 - enabling sha engine in mbedtls
+     * 1,273,548 - not using mbed implementation
+     *
+     * 24-Word
+     * 1,247,750 - default
+     * 1,863,712 - enabling sha engine in mbedtls
+     * 1,863,728 - not using mbed implementation
+     */
     CONFIDENTIAL uint512_t guess_master_seed_bin;
     CONFIDENTIAL hex512_t guess_master_seed_hex;
     jolt_err_t res;
-
+    int64_t t_start, t_end;
     /* Test 1 */
+    t_start = esp_timer_get_time();
     res = bm_mnemonic_to_master_seed(guess_master_seed_bin, 
-            "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about",
+            "abandon abandon abandon abandon abandon abandon abandon abandon "
+            "abandon abandon abandon about",
             "TREZOR");
+    t_end = esp_timer_get_time();
+    ESP_LOGI(TAG, "12-Word Mnemonic Master Seed Derived in %llu uS.", t_end-t_start);
     sodium_bin2hex(guess_master_seed_hex, sizeof(guess_master_seed_hex),
             guess_master_seed_bin, sizeof(guess_master_seed_bin));
     sodium_memzero(guess_master_seed_bin, sizeof(guess_master_seed_bin));
@@ -235,11 +254,14 @@ TEST_CASE("BIP39/44 Mnemonic To Master Seed", "[bip_mnemonic]"){
     sodium_memzero(guess_master_seed_hex, sizeof(guess_master_seed_hex));
 
     /* Test 2 */
+    t_start = esp_timer_get_time();
     res = bm_mnemonic_to_master_seed(guess_master_seed_bin, 
             " hamster diagram private dutch      cause delay private meat slide "
             "toddler\nrazor\t book happy fancy gospel tennis maple dilemma "
             "loan word    shrug inflict delay length   \n\t\n",
             "TREZOR");
+    t_end = esp_timer_get_time();
+    ESP_LOGI(TAG, "24-Word Mnemonic Master Seed Derived in %llu uS.", t_end-t_start);
     sodium_bin2hex(guess_master_seed_hex, sizeof(guess_master_seed_hex),
             guess_master_seed_bin, sizeof(guess_master_seed_bin));
     sodium_memzero(guess_master_seed_bin, sizeof(guess_master_seed_bin));
@@ -262,12 +284,17 @@ TEST_CASE("Mnemonic to Nano (165) Private Key", "[bip_mnemonic]"){
     CONFIDENTIAL uint256_t guess_private_key_bin;
     CONFIDENTIAL hex256_t guess_private_key_hex;
 
+    int64_t t_start, t_end;
+
     /* Derive Master Seed */
+    t_start = esp_timer_get_time();
     bm_mnemonic_to_master_seed(guess_master_seed_bin, 
             "edge defense waste choose enrich upon flee junk siren film clown "
             "finish luggage leader kid quick brick print evidence swap drill "
             "paddle truly occur",
             "some password");
+    t_end = esp_timer_get_time();
+    ESP_LOGI(TAG, "24-Word Mnemonic Master Seed Derived in %llu uS.", t_end-t_start);
     sodium_bin2hex(guess_master_seed_hex, sizeof(guess_master_seed_hex),
             guess_master_seed_bin, sizeof(guess_master_seed_bin));
     TEST_ASSERT_EQUAL_STRING(
@@ -276,8 +303,11 @@ TEST_CASE("Mnemonic to Nano (165) Private Key", "[bip_mnemonic]"){
             guess_master_seed_hex);
 
     /* Derive Nano Private Key */
+    t_start = esp_timer_get_time();
     bm_master_seed_to_private_key(guess_private_key_bin, guess_master_seed_bin,
             "ed25519 seed", 3, 44u | BM_HARDENED, 165u | BM_HARDENED, 0u | BM_HARDENED);
+    t_end = esp_timer_get_time();
+    ESP_LOGI(TAG, "Master Seed to Private Key in %llu uS.", t_end-t_start);
 
     sodium_bin2hex(guess_private_key_hex, sizeof(guess_private_key_hex),
             guess_private_key_bin, sizeof(guess_private_key_bin));
